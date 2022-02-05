be628fffebd408c07e6f54d4ade6bbbc1eb1b995
Christian Levin <christian.johansson@gmail.com>
2022-02-05 23:18:42 +0100

    Initiation of class and variables

--- AdventOfCode2015Day03/Program.cs
+++ AdventOfCode2015Day03/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AdventOfCode2015Day03
 {
     class Program
     {
+        class Location
+        {
+            public int Xcolumn { get; set; }
+            public int Yrow { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var santasPosition = new Location();
+            santasPosition.Xcolumn = 0;
+            santasPosition.Yrow = 0;
+            var visitedHouses = new List<Location>();
+            visitedHouses.Add(santasPosition);
+            var numberOfHousesVisited = 0;
+            var directions = File.ReadAllLines("c://AdventOfCode2015Day03PussleInput.txt");
+            Console.WriteLine(directions);
+            //var directions = File.currentWorking();
         }
     }
 }
